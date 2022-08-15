db90bb038ca635eb771f634ac2fa0ca4617afb5c
angurgapi <sophiekovalsky@gmail.com>
2022-08-15 17:00:05 +0300

    contact page

--- pages/contacts.vue
+++ pages/contacts.vue
@@ -1,5 +1,30 @@
 <template>
     <div class="page-wrapper__content">
+        <div class="contacts">
          <h2 class="festive">Get in touch</h2>
+         <div class="contacts__main">
+             <img src="pics/other/photo.jpg" />
+             <p>This is me, by the way</p>
+         </div>
+        </div>
     </div>
 </template>
+
+<style lang="scss" scoped>
+.contacts {
+    margin: auto;
+    width: fit-content;
+    &__main {
+        margin-top: 40px;
+        display: flex;
+        img {
+            height: 250px;
+            width: 250px;
+            border-radius: 50%;
+        }
+        p {
+            font-family: 'Zen';
+        }
+    }
+}
+</style>
