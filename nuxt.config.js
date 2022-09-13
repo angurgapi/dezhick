8ff98cc59c09c5dfb8df7165d84e50eb8c6af942
angurgapi <sophiekovalsky@gmail.com>
2022-09-13 17:06:41 +0300

    redeploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,6 +5,7 @@ export default {
     htmlAttrs: {
       lang: 'en'
     },
+    
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -15,6 +16,7 @@ export default {
       { rel: 'icon', type: 'image/x-icon', href: '/pics/icon.jpg' }
     ]
   },
+  target: 'static',
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
